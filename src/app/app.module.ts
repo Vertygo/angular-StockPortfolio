7326495cdde9295bd6e6105485b23c312f0eb801
Milan Svitlica <milan.svitlica@leversys.com>
2017-03-08 00:30:42 +0100

    basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+const routes: Routes = [
+    { path: '', component: PortfolioComponent },
+    { path: 'portfolio', component: PortfolioComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PortfolioComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]

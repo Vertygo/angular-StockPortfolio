abf44bc812798e94b7c6a93fd57ea72f58fc2660
Ivan Milosavljevic <verthygo@gmail.com>
2017-03-14 04:16:09 -0700

    add build script

---
+++ scripts/build/001_OutputCopyAndRemove.ps1
@@ -0,0 +1,4 @@
+﻿param([string]$sourcePath, [string]$targetPath) #Must be the first statement in your script
+
+Remove-Item -Recurse -Force ([IO.Path]::Combine($targetPath, "*"))
+Copy-Item ([IO.Path]::Combine($sourcePath, "dist\*")) $targetPath
